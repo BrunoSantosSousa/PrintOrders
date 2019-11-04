bc7956de841f1e06e3ddef7354538fb712640729
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2019-11-04 01:57:06 +0000

    Refactoring base controller to work with external validation rules

--- print-orders-api/app/Http/Controllers/Controller.php
+++ print-orders-api/app/Http/Controllers/Controller.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use App\Http\RequestRules\IRestfulRequestRule;
+
 
 class Controller extends BaseController
 {
-    //
+    private $requestRule;
+
+    public function __construct(IRestfulRequestRule $requestRule) {
+        $this->requestRule = $requestRule;
+    }
+
+    public function validatePost(Request $request) {
+        $this->validate($request, $this->requestRule->getPostRules());
+    }
+
+    public function validatePut(Request $request) {
+        $this->validate($request, $this->requestRule->getPutRules());
+    }
+
+    public function validateDestroy(Request $request) {
+        $this->validate($request, $this->requestRule->getDestroyRules());
+    }
+
 }
