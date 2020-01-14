c65d24c7d7cb262060ce469328afe6ed4b55a4ba
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2020-01-14 13:38:31 +0000

    Binding resources to provider

--- print-orders-api/app/Providers/AppServiceProvider.php
+++ print-orders-api/app/Providers/AppServiceProvider.php
@@ -18,6 +18,21 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(\App\Http\RequestRules\AuthRequestRule::class);
         $this->app->bind(\App\Http\RequestRules\GradeRequestRule::class);
 
+        // Resources
+        $this->app->bind(\App\Http\Resources\UserGradeResource::class);
+
+        // Resource Collections
+        $this->app->bind(\App\Http\Resources\IResourceCollection::class,
+                         \App\Http\Resources\ResourceCollection::class);
+
+        $this->app->bind('App/Http/Resources/UserGradeResourceCollection', function($app) {
+            $userGradeResource = $app->make(\App\Http\Resources\UserGradeResource::class);
+            return $app->makeWith(\App\Http\Resources\IResourceCollection::class, [
+                'resource' => $userGradeResource
+            ]);
+        });
+
+
         // Services
         $this->app->bind(\App\Services\IUidGenerator::class, \App\Services\UidGenerator::class);
 
