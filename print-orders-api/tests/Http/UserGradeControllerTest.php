e54eac5d0d9b8409d121ac3d346660df3ab3a676
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2020-02-27 17:23:47 +0000

    Testing POST and DELETE routes of UserGradeController

--- print-orders-api/tests/Http/UserGradeControllerTest.php
+++ print-orders-api/tests/Http/UserGradeControllerTest.php
@@ -28,4 +28,34 @@ class UserGradeControllerTest extends TestCase
         $response = $this->actingAs($user)->call('GET', "/api/user/{$user->id}/grade");
         $this->assertEquals(200, $response->status());
     }
+
+    /**
+     * UserGradeController post methd MUST create a new user_grade row.
+     *
+     * @return void
+     */
+    public function testPostRequest()
+    {
+        $admin = $this->getAdminUser();
+        $user = factory(\App\User::class)->create();
+        $grade = factory(\App\Grade::class)->create();
+        $data = [
+            'grade_id' => $grade->id
+        ];
+        $response = $this->actingAs($admin)->call('POST', "/api/user/{$user->id}/grade/", $data);
+        $this->assertEquals(200, $response->status());
+    }
+
+    /**
+     * UserGradeController post MUST delete a row from database.
+     *
+     * @return void
+     */
+    public function testDeleteRequest()
+    {
+        $admin = $this->getAdminUser();
+        $userGrade = factory(\App\UserGrade::class)->create();
+        $response = $this->actingAs($admin)->call("DELETE", "/api/user/{$userGrade->user_id}/grade/{$userGrade->id}");
+        $this->assertEquals(200, $response->status());
+    }
 }
