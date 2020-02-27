abe5f2ad8965dae0e5af6b2e6ae05b530668e50e
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2020-02-27 17:19:19 +0000

    Implementing UserGrade POST and DELETE routes

--- print-orders-api/app/Http/Controllers/UserGradeController.php
+++ print-orders-api/app/Http/Controllers/UserGradeController.php
@@ -26,4 +26,32 @@ class UserGradeController extends Controller
             return response()->json(['message' => "Failed to load user's grade data.", 'stack' => $e->getMessage()], 409);
         }
     }
+
+    public function post(Request $request, $userId)
+    {
+        $this->validatePost($request);
+        try {
+            $rows = UserGrade::where([['user_id', '=', $userId], ['grade_id' , '=', $request->input('grade_id')]])->get();
+            if($rows->count() > 0) {
+                return response()->json(['message' => 'DUPLICATED'], 409);
+            }
+            $userGrade = UserGrade::create([
+                'user_id' => $userId,
+                'grade_id' => $request->input('grade_id')
+            ]);
+            return response()->json(['user_grade' => $userGrade, 'message' => "CREATED"]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => "UserGrade registration failed!"]);
+        }
+    }
+
+    public function delete($userId, $gradeId)
+    {
+        try {
+            UserGrade::destroy($gradeId);
+            return response()->json(['message' => "DELETED"]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => "Failed to destroy UserGrade!"]);
+        }
+    }
 }
