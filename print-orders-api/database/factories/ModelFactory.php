d987aeba77ff794b8aeb4c6275a90b6224cd68e9
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2020-02-27 17:21:41 +0000

    Implementing new model factories

--- print-orders-api/database/factories/ModelFactory.php
+++ print-orders-api/database/factories/ModelFactory.php
@@ -40,3 +40,44 @@ $factory->define(App\Grade::class, function(Faker\Generator $faker) {
         ])
     ];
 });
+
+$factory->define(App\UserGrade::class, function(Faker\Generator $faker) {
+    return [
+        'user_id' => factory(App\User::class)->create()->id,
+        'grade_id' => factory(App\Grade::class)->create()->id
+    ];
+});
+
+$factory->define(App\Order::class, function(Faker\Generator $faker) {
+    return [
+        'grade_id' => factory(App\Grade::class)->create()->id,
+        'type' => $faker->randomElement(['xerox', 'book', 'test']),
+        'status' => 'pending',
+        'checked' => 0,
+        'comments' => '',
+        'drive_path' => '',
+        'delivery_date' => '2020-02-10'
+    ];
+});
+
+$factory->define(App\XeroxOrder::class, function(Faker\Generator $faker) {
+    return [
+        'order_id' => factory(App\Order::class)->create(['type' => 'xerox'])->id,
+        'description' => 'random description'
+    ];
+});
+
+$factory->define(App\TestOrder::class, function(Faker\Generator $faker) {
+    return [
+        'order_id' => factory(App\Order::class)->create(['type' => 'test'])->id,
+        'description' => 'random description'
+    ];
+});
+
+$factory->define(App\BookOrder::class, function(Faker\Generator $faker){
+    return [
+        'order_id' => factory(App\Order::class)->create(['type' => 'book'])->id,
+        'book_name' => 'Trilhas',
+        'pages' => '1, 2 e 3'
+    ];
+});
