83b8524855855a5ff810008767a2738a606606d4
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2020-02-27 17:31:10 +0000

    Refactoring to add new features

--- print-orders-app/src/Layout/Title/index.js
+++ print-orders-app/src/Layout/Title/index.js
@@ -3,20 +3,25 @@ import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 
 const useStyle = makeStyles(theme => ({
-    root: {
+    withMargin: {
         padding: '0px 5px 10px',
         width: '100%',
         marginBottom: '10px',
+        borderBottom: `1px solid ${theme.palette.divider}`
+    },
+    withoutMargin: {
+        padding: '0px 5px 10px',
+        width: '100%',
         borderBottom: `1px solid ${theme.palette.divider}`
     }
 }))
 
 export default function Title(props) {
-    const { children } = props
+    const { margin, children } = props
     const classes = useStyle()
 
     return (
-        <Typography variant="h5" className={classes.root}>
+        <Typography variant="h5" className={margin ? classes.withMargin : classes.withoutMargin}>
             {children}
         </Typography>
     )
